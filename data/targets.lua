51b0792f5ada073597d185fa9ed55a4e3abda082
Luke <sabolukas03@gmail.com>
2022-08-16 23:42:18 +0200

    feat: initial file structure and setup

---
+++ data/targets.lua
@@ -0,0 +1,8 @@
+targets = {
+    {
+        -- Legion fleeca
+        coords = vector3(149.1625, -1041.4609, 29.3741),
+        size = vector3(2, 1.3, 2),
+        rotation = 161.4161
+    }
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,43 @@
+--[[ FX Information ]]--
+fx_version   'cerulean'
+use_experimental_fxv2_oal 'yes'
+lua54        'yes'
+game         'gta5'
+
+--[[ Resource Information ]]--
+name         'ox_banking'
+version      '0.0.0'
+description  'Banking system for ox_core'
+license      'MIT'
+author       'overextended'
+repository   'https://github.com/overextended/ox_banking'
+
+--[[ Manifest ]]--
+dependencies {
+	'/server:5104',
+	'/onesync',
+}
+
+shared_scripts {
+	'@ox_lib/init.lua',
+}
+
+client_scripts {
+    '@ox_core/imports/client.lua',
+    'client/client.lua',
+    'data/locations.lua',
+    'data/targets.lua'
+}
+
+server_scripts {
+	'@oxmysql/lib/MySQL.lua',
+	'@ox_core/imports/server.lua',
+	'server/server.lua'
+}
+
+ui_page 'web/build/index.html'
+
+files {
+    'web/build/index.html',
+    'web/build/**/*',
+}
