2fc084e083f085e1e1bcc9d81c8497773130b175
Luke <sabolukas03@gmail.com>
2022-08-17 15:42:07 +0200

    feat(client): target zones and locations

--- client/client.lua
+++ client/client.lua
@@ -1,5 +1,14 @@
 local oxTarget = GetConvar('ox_enableTarget', 'false') == 'true'
 
+local ATMProps = {
+	`prop_atm_01`,
+	`prop_atm_02`,
+	`prop_atm_03`,
+	`prop_fleeca_atm`,
+	`v_5_b_atm1`,
+	`v_5_b_atm2`
+}
+
 local function createBankBlip(coords)
     local blip = AddBlipForCoord(coords.x, coords.y, coords.z)
     SetBlipSprite(blip, 207)
@@ -26,5 +35,37 @@ if not oxTarget then
             end
         end
         createBankBlip(location)
+    end
+else
+    exports.ox_target:addModel(ATMProps, {
+        {
+            name = 'access_atm',
+            icon = 'fa-solid fa-money-check',
+            label = 'Access ATM',
+            onSelect = function()
+                print('open atm ui')
+            end,
+        }
+    })
+    for i = 1, #targets do
+        exports.ox_target:addBoxZone({
+            coords = targets[i].coords,
+            size = targets[i].size,
+            rotation = targets[i].rotation,
+            debug = true,
+            interactionDistance = 1.3,
+            drawSprite = true,
+            options = {
+                {
+                    name = 'access_bank',
+                    icon = 'fa-solid fa-dollar-sign',
+                    label = 'Access bank',
+                    onSelect = function()
+                        print('open bank ui')
+                    end,
+                }
+            }
+        })
+        createBankBlip(targets[i].coords)
     end
 end
