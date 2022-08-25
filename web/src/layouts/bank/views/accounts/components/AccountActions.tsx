42d2a6bddede2a0079b97e652e77722827d3edf1
Luke <sabolukas03@gmail.com>
2022-08-25 17:19:22 +0200

    feat(web): logs button account setting

--- web/src/layouts/bank/views/accounts/components/AccountActions.tsx
+++ web/src/layouts/bank/views/accounts/components/AccountActions.tsx
@@ -1,7 +1,15 @@
 import { Button, Paper, SimpleGrid, Stack, Text } from '@mantine/core';
 import { FaExternalLinkAlt } from 'react-icons/fa';
+import { useNavigate } from 'react-router-dom';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
+import { selectedLogsAccountAtom, selectedAccountAtom, accountsAtom } from '../../../../../atoms/account';
 
 const AccountActions: React.FC = () => {
+  const navigate = useNavigate();
+  const setSelectedLogsAccount = useSetRecoilState(selectedLogsAccountAtom);
+  const selectedAccount = useRecoilValue(selectedAccountAtom);
+  const accounts = useRecoilValue(accountsAtom);
+
   return (
     <Paper p="md">
       <Stack>
@@ -10,7 +18,14 @@ const AccountActions: React.FC = () => {
           <Button uppercase>Withdraw</Button>
           <Button uppercase>Deposit</Button>
           <Button uppercase>Transfer</Button>
-          <Button uppercase rightIcon={<FaExternalLinkAlt />}>
+          <Button
+            uppercase
+            rightIcon={<FaExternalLinkAlt />}
+            onClick={() => {
+              setSelectedLogsAccount(accounts[selectedAccount || 0].id);
+              navigate('/logs');
+            }}
+          >
             Logs
           </Button>
         </SimpleGrid>
