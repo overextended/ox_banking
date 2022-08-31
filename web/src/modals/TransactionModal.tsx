426b8bbeec1226f84f057ba3a2a8adebde6dd423
Luke <sabolukas03@gmail.com>
2022-08-31 12:49:56 +0200

    style(web): tweak transaction modal element sizes

--- web/src/modals/TransactionModal.tsx
+++ web/src/modals/TransactionModal.tsx
@@ -8,21 +8,21 @@ const TransactionModal: React.FC<{ transaction: TransactionProps }> = ({ transac
       <Text size="xs">Transaction ID: 197751331194</Text>
       <Text size="xs">Date: {transaction.date}</Text>
       <Box>
-        <Text size="xl">{transaction.type === 'inbound' ? 'From' : 'To'} account</Text>
+        <Text>{transaction.type === 'inbound' ? 'From' : 'To'} account</Text>
         <Text size="sm">
           {transaction.accountOwner} - {transaction.accountId}
         </Text>
       </Box>
       <Box>
-        <Text size="xl">Money {transaction.type === 'inbound' ? 'received' : 'sent'}</Text>
-        <Text size="sm" color={transaction.type === 'inbound' ? 'teal' : 'red'}>
+        <Text>Money {transaction.type === 'inbound' ? 'received' : 'sent'}</Text>
+        <Text size="sm" weight={700} color={transaction.type === 'inbound' ? 'teal' : 'red'}>
           {transaction.type === 'inbound' ? '+' : '-'}
           {formatNumber(transaction.amount)}
         </Text>
       </Box>
       {transaction.message && (
         <Box>
-          <Text size="xl">Message</Text>
+          <Text>Message</Text>
           <Text size="sm">{transaction.message}</Text>
         </Box>
       )}
