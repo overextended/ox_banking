9813a7c3abb797571487734914dc2e96116094d3
Luke <39926192+LukeWasTakenn@users.noreply.github.com>
2023-10-30 20:21:53 +0100

    refactor(web): initial account actions layout

--- web/src/layouts/bank/pages/accounts/Accounts.tsx
+++ web/src/layouts/bank/pages/accounts/Accounts.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import BaseCard from '@/layouts/bank/components/BaseCard';
-import { ChevronLeft, ChevronRight, CreditCard, Plus } from 'lucide-react';
-import { formatNumber } from '@/utils/formatNumber';
+import {
+  ChevronLeft,
+  ChevronRight,
+  Coins,
+  CreditCard,
+  Plus,
+  ScanText,
+  Settings,
+  Wallet,
+  Percent,
+  PiggyBank, Repeat, Landmark, Copy, History,
+} from 'lucide-react';
 import AccountCard from '@/layouts/bank/pages/accounts/components/AccountCard';
 import { Button } from '@/components/ui/button';
+import { formatNumber } from '@/utils/formatNumber';
+import { Tooltip, TooltipTrigger, TooltipContent } from '@/components/ui/tooltip';
 
 const Accounts: React.FC = () => {
   return (
-    <div className='w-full h-full p-2 flex flex-col overflow-hidden'>
+    <div className='w-full h-full p-2 gap-2 flex flex-col overflow-hidden'>
       <BaseCard title='Accounts' icon={CreditCard} className='overflow-visible'>
         <div className='flex w-full gap-4 items-center justify-center'>
           <Button size='icon' className='rounded-full' disabled>
@@ -30,7 +42,87 @@ const Accounts: React.FC = () => {
             <ChevronRight />
           </Button>
         </div>
+        <div className='flex gap-2 w-full items-center justify-center'>
+          <div className='rounded-full w-[5px] h-[5px] bg-primary' />
+          <div className='rounded-full w-[5px] h-[5px] bg-muted' />
+          <div className='rounded-full w-[5px] h-[5px] bg-muted' />
+        </div>
       </BaseCard>
+      <div className='flex w-full h-full gap-2'>
+        <BaseCard title='Selected account' icon={ScanText} className='flex-1'>
+          <div className='flex justify-between'>
+            <div className='flex flex-col gap-4 flex-1'>
+              <div className='flex flex-col'>
+                <p className='text-muted-foreground text-xs'>Account name</p>
+                <p>Personal</p>
+              </div>
+              <div className='flex flex-col'>
+                <p className='text-muted-foreground text-xs'>Account type</p>
+                <p>Personal account</p>
+              </div>
+              <div className='flex flex-col'>
+                <p className='text-muted-foreground text-xs'>Account number</p>
+                <div className='flex gap-2 items-center'>
+                  <p>{Math.trunc(Math.random() * 1000000)}</p>
+                  <Tooltip delayDuration={200}>
+                    <TooltipTrigger>
+                      <button className='flex items-center'>
+                        <Copy size={14} />
+                      </button>
+                    </TooltipTrigger>
+                    <TooltipContent>
+                      Copy
+                    </TooltipContent>
+                  </Tooltip>
+                </div>
+              </div>
+              <div className='flex flex-col'>
+                <p className='text-muted-foreground text-xs'>Account owner</p>
+                <p>Jack Sparrow</p>
+              </div>
+              <div className='flex flex-col'>
+                <p className='text-muted-foreground text-xs'>Disposable amount</p>
+                <p>{formatNumber(Math.trunc(Math.random() * 1000000))}</p>
+              </div>
+            </div>
+            <div className='flex flex-col flex-1 gap-4'>
+              <Button
+                variant='secondary'
+                className='flex flex-col h-fit gap-1 items-center'>
+                <Wallet />
+                Withdraw
+              </Button>
+              <Button
+                variant='secondary'
+                className='flex flex-col h-fit gap-1 items-center'>
+                <Landmark />
+                Deposit
+              </Button>
+              <Button
+                variant='secondary'
+                className='flex flex-col h-fit gap-1 items-center'>
+                <Repeat />
+                Transfer
+              </Button>
+              <Button
+                variant='secondary'
+                className='flex flex-col h-fit gap-1 items-center'>
+                <History />
+                Account logs
+              </Button>
+            </div>
+          </div>
+        </BaseCard>
+        <BaseCard title='Settings' icon={Settings} className='flex-1'>
+          <div className='flex flex-col gap-2'>
+            <Button variant='secondary'>Transfer ownership</Button>
+            <Button variant='secondary'>Convert to shared</Button>
+            <Button variant='secondary'>Rename</Button>
+            <Button variant='secondary'>Manage permissions</Button>
+            <Button variant='destructive'>Delete account</Button>
+          </div>
+        </BaseCard>
+      </div>
     </div>
   );
 };
