d3189c17d143844714274bffa2f26bb9ba3daf19
Luke <39926192+LukeWasTakenn@users.noreply.github.com>
2023-11-11 12:38:36 +0100

    chore: remove unused directories

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -23,17 +23,12 @@ shared_scripts {
 }
 
 client_scripts {
-    '@ox_core/imports/client.lua',
-    'data/locations.lua',
-    'data/targets.lua',
     'client/client.lua',
     'dist/client.js'
 }
 
 server_scripts {
     '@oxmysql/lib/MySQL.lua',
-    '@ox_core/imports/server.lua',
-    'server/server.lua',
     'dist/server.js'
 }
 
