5658a57a360d0b463e307f515baf324c3e1ba35a
Luke <39926192+LukeWasTakenn@users.noreply.github.com>
2024-02-02 14:33:35 +0100

    refactor(web): remove logs page from navbar

--- web/src/layouts/bank/components/Navbar.tsx
+++ web/src/layouts/bank/components/Navbar.tsx
@@ -15,7 +15,6 @@ const Navbar: React.FC = () => {
       <div className="flex flex-col items-center justify-center">
         <NavLink icon={LayoutDashboard} label={locales.dashboard} path="/" />
         <NavLink icon={CreditCard} label={locales.accounts} path="/accounts" />
-        <NavLink icon={HistoryIcon} label={locales.logs} path="/logs" />
       </div>
 
       <div className="flex flex-col items-center justify-center">
