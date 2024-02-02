6d0746fedeec5c77f638408d59c57ebd711e7c4a
Linden <65407488+thelindat@users.noreply.github.com>
2024-02-02 13:22:50 +1100

    chore: update package and build process

--- build.js
+++ build.js
@@ -1,41 +1,103 @@
-import { build } from 'esbuild';
+import esbuild from 'esbuild';
+import { readFileSync, writeFileSync } from 'fs';
 
-const production = process.argv.findIndex((argItem) => argItem === '--mode=production') >= 0;
-
-const onRebuild = (context) => {
-  return async (err, res) => {
-    if (err) {
-      return console.error(`[${context}]: Rebuild failed`, err);
-    }
-
-    console.log(`[${context}]: Rebuild succeeded, warnings:`, res.warnings);
-  };
-};
-
+/** @type {import('esbuild').BuildOptions} */
 const server = {
   platform: 'node',
   target: ['node16'],
   format: 'cjs',
 };
 
+/** @type {import('esbuild').BuildOptions} */
 const client = {
   platform: 'browser',
   target: ['chrome93'],
   format: 'iife',
 };
 
+const production = process.argv.includes('--mode=production');
+const buildCmd = production ? esbuild.build : esbuild.context;
+const packageJson = JSON.parse(readFileSync('package.json', { encoding: 'utf8' }));
+
+writeFileSync(
+  '.yarn.installed',
+  new Date().toLocaleString('en-AU', {
+    timeZone: 'UTC',
+    timeStyle: 'long',
+    dateStyle: 'full',
+  })
+);
+
+writeFileSync(
+  'fxmanifest.lua',
+  `fx_version 'cerulean'
+game 'gta5'
+use_experimental_fxv2_oal 'yes'
+lua54 'yes'
+
+name '${packageJson.name}'
+author '${packageJson.author}'
+version '${packageJson.version}'
+license '${packageJson.license}'
+repository '${packageJson.repository.url}'
+description '${packageJson.description}'
+
+dependencies {
+    '/server:7290',
+    '/onesync',
+}
+
+client_scripts {
+  '@ox_lib/init.lua',
+  'client/client.lua',
+  'dist/client.js'
+}
+
+server_scripts {
+  '@oxmysql/lib/MySQL.lua',
+  'dist/server.js'
+}
+
+
+ui_page 'web/build/index.html'
+
+files {
+    'locales/*.json',
+    'data/*.json',
+    'web/build/index.html',
+    'web/build/**/*',
+}
+
+`
+);
+
 for (const context of ['client', 'server']) {
-  build({
+  buildCmd({
     bundle: true,
-    entryPoints: [`${context}/${context}.ts`],
+    entryPoints: [`${context}/index.ts`],
     outfile: `dist/${context}.js`,
-    watch: production
-      ? false
-      : {
-          onRebuild: onRebuild(context),
-        },
+    keepNames: true,
+    dropLabels: production ? ['DEV'] : undefined,
+    legalComments: 'inline',
+    plugins: production
+      ? undefined
+      : [
+          {
+            name: 'rebuild',
+            setup(build) {
+              const cb = (result) => {
+                if (!result || result.errors.length === 0) console.log(`Successfully built ${context}`);
+              };
+              build.onEnd(cb);
+            },
+          },
+        ],
     ...(context === 'client' ? client : server),
   })
-    .then(() => console.log(`[${context}]: Built successfully!`))
+    .then((build) => {
+      if (production) return console.log(`Successfully built ${context}`);
+
+      build.watch();
+    })
     .catch(() => process.exit(1));
 }
