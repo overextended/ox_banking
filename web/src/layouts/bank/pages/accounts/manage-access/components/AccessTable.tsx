eeadfbee8c1c7adde78d8022342f8b1dddc4d7a4
Luke <39926192+LukeWasTakenn@users.noreply.github.com>
2024-07-30 14:56:03 +0200

    fix(web): disable access buttons for owner and same role users

--- web/src/layouts/bank/pages/accounts/manage-access/components/AccessTable.tsx
+++ web/src/layouts/bank/pages/accounts/manage-access/components/AccessTable.tsx
@@ -22,22 +22,39 @@ const AccessTable: React.FC<{ accountId: number }> = ({ accountId }) => {
   const { role: characterRole } = useActiveAccount()!;
 
   const { data, isLoading } = useQuery<AccessTableData>({
-    queryKey: ['account-access', tableFilter.page, tableFilter.search], queryFn: async () => {
-      const resp = await fetchNui<AccessTableData>('getAccountUsers', {
-        accountId: +accountId!, page: tableFilter.page, search: tableFilter.search,
-      }, {
-        data: {
-          numberOfPages: 2,
-          users: [
-            {
-              name: 'Place Holder',
-              stateId: 'LF23312',
-              role: 'manager',
-            },
-          ],
+    queryKey: ['account-access', tableFilter.page, tableFilter.search],
+    queryFn: async () => {
+      const resp = await fetchNui<AccessTableData>(
+        'getAccountUsers',
+        {
+          accountId: +accountId!,
+          page: tableFilter.page,
+          search: tableFilter.search,
         },
-        delay: 2500,
-      });
+        {
+          data: {
+            numberOfPages: 2,
+            users: [
+              {
+                name: 'Place Holder',
+                stateId: 'LF23312',
+                role: 'manager',
+              },
+              {
+                name: 'Place Holder',
+                stateId: 'LF23312',
+                role: 'owner',
+              },
+              {
+                name: 'Place Holder',
+                stateId: 'LF23312',
+                role: 'contributor',
+              },
+            ],
+          },
+          delay: 2500,
+        }
+      );
 
       setNumberOfPages(resp.numberOfPages);
 
@@ -48,38 +65,48 @@ const AccessTable: React.FC<{ accountId: number }> = ({ accountId }) => {
   const spinnerVisible = isLoading || isSearchDebouncing;
 
   return (
-    <div
-      className='flex flex-col justify-between h-full border border-border rounded-lg p-4'>
-
-      {!spinnerVisible ? <div>
-        <div className='grid grid-cols-4 place-items-center text-sm'>
-          <p>{locales.name}</p>
-          <p>{locales.role}</p>
-          <p>{locales.state_id}</p>
-          <p></p>
+    <div className="border-border flex h-full flex-col justify-between rounded-lg border p-4">
+      {!spinnerVisible ? (
+        <div>
+          <div className="grid grid-cols-4 place-items-center text-sm">
+            <p>{locales.name}</p>
+            <p>{locales.role}</p>
+            <p>{locales.state_id}</p>
+            <p></p>
+          </div>
+          {data?.users.map((user) => (
+            <AccessTableUserItem
+              key={user.stateId}
+              characterRole={characterRole}
+              accountId={accountId}
+              name={user.name}
+              stateId={user.stateId}
+              role={user.role}
+            />
+          ))}
         </div>
-        {data?.users.map(user => (
-          <AccessTableUserItem key={user.stateId} characterRole={characterRole} accountId={accountId} name={user.name}
-                               stateId={user.stateId}
-                               role={user.role} />
-        ))}
-      </div> : (
-        <div className='flex items-center justify-center h-full'>
-          <SpinningLoader variant='primary' />
+      ) : (
+        <div className="flex h-full items-center justify-center">
+          <SpinningLoader variant="primary" />
         </div>
       )}
-      <div className='flex gap-4 items-center justify-end'>
-        <Button size='icon' onClick={() => setTableFilter(prev => ({ ...prev, page: --prev.page }))}
-                disabled={tableFilter.page <= 0 || spinnerVisible}>
+      <div className="flex items-center justify-end gap-4">
+        <Button
+          size="icon"
+          onClick={() => setTableFilter((prev) => ({ ...prev, page: --prev.page }))}
+          disabled={tableFilter.page <= 0 || spinnerVisible}
+        >
           <ChevronLeft size={20} />
         </Button>
         <p>{locales.current_page.format(tableFilter.page + 1, spinnerVisible ? '?' : numberOfPages)}</p>
-        <Button size='icon' onClick={() => setTableFilter(prev => ({ ...prev, page: ++prev.page }))}
-                disabled={tableFilter.page >= numberOfPages - 1 || spinnerVisible}>
+        <Button
+          size="icon"
+          onClick={() => setTableFilter((prev) => ({ ...prev, page: ++prev.page }))}
+          disabled={tableFilter.page >= numberOfPages - 1 || spinnerVisible}
+        >
           <ChevronRight size={20} />
         </Button>
       </div>
-
     </div>
   );
 };
--- web/src/layouts/bank/pages/accounts/manage-access/components/AccessTableUserItem.tsx
+++ web/src/layouts/bank/pages/accounts/manage-access/components/AccessTableUserItem.tsx
@@ -11,30 +11,44 @@ const AccessTableUserItem: React.FC<AccessTableUser & { accountId: number; chara
   const { role, name, stateId, characterRole, accountId } = props;
   const modal = useModal();
 
-  const ROLES = React.useMemo(() => ({
-    owner: locales.owner,
-    manager: locales.manager,
-    contributor: locales.contributor,
-  }), []);
+  const ROLES = React.useMemo(
+    () => ({
+      owner: locales.owner,
+      manager: locales.manager,
+      contributor: locales.contributor,
+    }),
+    []
+  );
 
   return (
-    <div className='grid grid-cols-4 py-4 place-items-center border-t border-border'>
+    <div className="border-border grid grid-cols-4 place-items-center border-t py-4">
       <p>{name}</p>
       <p>{ROLES[role]}</p>
       <p>{stateId}</p>
-      <div className='flex gap-2 items-center'>
-        <Button disabled={characterRole !== 'owner'} size='icon' onClick={() => modal.open({
-          title: locales.manage_access,
-          children: <ManageUserModal accountId={accountId} targetStateId={stateId} defaultRole={role} />,
-        })}>
+      <div className="flex items-center gap-2">
+        <Button
+          disabled={role === 'owner' || role === characterRole}
+          size="icon"
+          onClick={() =>
+            modal.open({
+              title: locales.manage_access,
+              children: <ManageUserModal accountId={accountId} targetStateId={stateId} defaultRole={role} />,
+            })
+          }
+        >
           <UserCog size={20} />
         </Button>
-        <Button disabled={(characterRole !== 'owner' && characterRole !== 'manager') || role === characterRole}
-                size='icon' variant='destructive'
-                onClick={() => modal.open({
-                  title: locales.remove_user,
-                  children: <RemoveUserModal targetStateId={stateId} accountId={accountId} />,
-                })}>
+        <Button
+          disabled={role === 'owner' || role === characterRole}
+          size="icon"
+          variant="destructive"
+          onClick={() =>
+            modal.open({
+              title: locales.remove_user,
+              children: <RemoveUserModal targetStateId={stateId} accountId={accountId} />,
+            })
+          }
+        >
           <UserMinus size={20} />
         </Button>
       </div>
