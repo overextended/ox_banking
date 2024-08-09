10d9813e2f71dfcb010660c0b1fe1e37cd60aa17
Luke <39926192+LukeWasTakenn@users.noreply.github.com>
2024-08-09 11:59:08 +0200

    chore(locales): logs table header locales

--- src/web/src/layouts/bank/pages/accounts/logs/components/LogsTableHead.tsx
+++ src/web/src/layouts/bank/pages/accounts/logs/components/LogsTableHead.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import locales from '@/locales';
 
 const LogsTableHead: React.FC = () => {
   return (
     <div className="text-muted-foreground mb-2 grid grid-cols-[36px,repeat(5,1fr)] gap-2 border-b py-2 text-center text-sm">
       <p></p>
-      <p>Name</p>
-      <p>Message</p>
-      <p>Amount</p>
-      <p>New balance</p>
-      <p>Timestamp</p>
+      <p>{locales.name}</p>
+      <p>{locales.message}</p>
+      <p>{locales.amount}</p>
+      <p>{locales.new_balance}</p>
+      <p>{locales.date}</p>
     </div>
   );
 };
