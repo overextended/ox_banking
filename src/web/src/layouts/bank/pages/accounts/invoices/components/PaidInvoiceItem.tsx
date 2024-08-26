1e3f7a9ed4cff51d88c75d15b35c6c052a71156c
Luke <39926192+LukeWasTakenn@users.noreply.github.com>
2024-08-26 15:37:09 +0200

    refactor(web/invoices): redesign paid invoices

---
+++ src/web/src/layouts/bank/pages/accounts/invoices/components/PaidInvoiceItem.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { CheckCircle2 } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+import { useModal } from '@/components/ModalsProvider';
+import PaidInvoiceDetailsModal from '../modals/PaidInvoiceDetailsModal';
+
+const PaidInvoiceItem: React.FC = () => {
+  const modal = useModal();
+
+  return (
+    <div className="flex items-center justify-between gap-4 rounded-lg border p-4 shadow">
+      <div className="flex items-center gap-4">
+        <div className="text-secondary-foreground h-9 w-9 rounded-lg bg-green-400/20 p-2 text-green-700 dark:bg-green-500/20 dark:text-green-500">
+          <CheckCircle2 size={20} />
+        </div>
+        <div className="flex flex-col gap-1">
+          <p>SomeCompany LLC</p>
+          <p className="text-muted-foreground text-xs">Paid at: 12/03/2025</p>
+        </div>
+      </div>
+
+      <div className="flex items-center gap-2">
+        <Button
+          variant="outline"
+          onClick={() => modal.open({ title: 'Invoice details', children: <PaidInvoiceDetailsModal /> })}
+        >
+          Details
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default PaidInvoiceItem;
---
+++ src/web/src/layouts/bank/pages/accounts/invoices/modals/PaidInvoiceDetailsModal.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { formatNumber } from '@/utils/formatNumber';
+
+const PaidInvoiceDetailsModal: React.FC = () => {
+  return (
+    <div className="flex flex-col gap-4">
+      <div>
+        <p className="text-muted-foreground text-xs">Payment to</p>
+        <p className="text-sm">SomeCompany LLC</p>
+      </div>
+      <div>
+        <p className="text-muted-foreground text-xs">Due date</p>
+        <p>20/03/2025 13:40</p>
+      </div>
+      <div>
+        <p className="text-muted-foreground text-xs">Paid at</p>
+        <p>20/03/2025 13:40</p>
+      </div>
+      <div>
+        <p className="text-muted-foreground text-xs">Paid by</p>
+        <p>Some Name</p>
+      </div>
+      <div>
+        <p className="text-muted-foreground text-xs">Message</p>
+        <p className="text-sm">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam blanditiis consequatur cum cupiditate ea
+          error expedita facere ipsa ipsam laudantium magni maxime modi molestias odio odit pariatur perspiciatis
+          quisquam, rem.
+        </p>
+      </div>
+      <div>
+        <p className="text-muted-foreground text-xs">Total</p>
+        <p className="text-sm">{formatNumber(15700)}</p>
+      </div>
+    </div>
+  );
+};
+
+export default PaidInvoiceDetailsModal;
