fdcce309bdfa13b6d67b746e18cb04da7f238b00
Linden <65407488+thelindat@users.noreply.github.com>
2024-09-18 01:31:36 +1000

    refactor(web): use root package and esmodules

--- src/web/tailwind.config.js
+++ src/web/tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   darkMode: ['class'],
   content: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}', './app/**/*.{ts,tsx}', './src/**/*.{ts,tsx}'],
   theme: {
