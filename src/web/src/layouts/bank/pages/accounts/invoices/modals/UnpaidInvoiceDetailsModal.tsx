c00f1d5a8e5cb3a1c250e64aa13039d1ef094ff1
Skryptific <48567607+Skryptific@users.noreply.github.com>
2024-09-18 21:14:53 +0200

    feat(web/invoices): locales and display sentBy and sentAt

--- src/web/src/layouts/bank/pages/accounts/invoices/modals/UnpaidInvoiceDetailsModal.tsx
+++ src/web/src/layouts/bank/pages/accounts/invoices/modals/UnpaidInvoiceDetailsModal.tsx
@@ -1,3 +1,4 @@
+import locales from '@/locales';
 import { formatDate } from '@/utils/formatDate';
 import { formatNumber } from '@/utils/formatNumber';
 import React from 'react';
@@ -7,19 +8,29 @@ const UnpaidInvoiceDetailsModal: React.FC<{ invoice: UnpaidInvoice }> = ({ invoi
   return (
     <div className="flex flex-col gap-4">
       <div>
-        <p className="text-muted-foreground text-xs">Payment to</p>
+        <p className="text-muted-foreground text-xs">{locales.invoice_details_sent_to}</p>
         <p className="text-sm">{invoice.label}</p>
       </div>
+      {invoice.sentBy && (
+        <div>
+          <p className="text-muted-foreground text-xs">{locales.invoice_details_sent_by}</p>
+          <p className="text-sm">{invoice.sentBy}</p>
+        </div>
+      )}
       <div>
-        <p className="text-muted-foreground text-xs">Due date</p>
+        <p className="text-muted-foreground text-xs">{locales.invoice_details_sent_at}</p>
+        <p className="text-sm">{formatDate(invoice.sentAt)}</p>
+      </div>
+      <div>
+        <p className="text-muted-foreground text-xs">{locales.invoice_details_due_by}</p>
         <p>{formatDate(invoice.dueDate)}</p>
       </div>
       <div>
-        <p className="text-muted-foreground text-xs">Message</p>
+        <p className="text-muted-foreground text-xs">{locales.message}</p>
         <p className="text-sm">{invoice.message}</p>
       </div>
       <div>
-        <p className="text-muted-foreground text-xs">Total</p>
+        <p className="text-muted-foreground text-xs">{locales.invoice_total}</p>
         <p className="text-sm">{formatNumber(invoice.amount)}</p>
       </div>
     </div>
